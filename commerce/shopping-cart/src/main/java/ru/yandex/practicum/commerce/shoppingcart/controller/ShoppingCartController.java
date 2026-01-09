388fd510be6b460a2fb989238d956879e88a5af4
Utik24 <166395457+Utik24@users.noreply.github.com>
2026-01-09 22:30:18 +0300

    внесены первые правки

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shoppingcart/controller/ShoppingCartController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shoppingcart/controller/ShoppingCartController.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.commerce.shoppingcart.controller;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class ShoppingCartController implements ShoppingCartClient {
     }
 
     @Override
-    public ShoppingCartDto addToCart(Map<UUID, Integer> products,
+    public ShoppingCartDto addToCart(@RequestBody Map<UUID, Integer> products,
                                      @RequestParam(name = "username") String username) throws FeignException {
         log.info("{} добавил товар {} в корзину.", username, products);
         return cartService.addToCartProduct(username, products);
@@ -41,12 +42,14 @@ public class ShoppingCartController implements ShoppingCartClient {
     }
 
     @Override
-    public ShoppingCartDto removeFromCart(List<UUID> products, @RequestParam String username) throws FeignException {
+    public ShoppingCartDto removeFromCart(@RequestBody List<UUID> products,
+                                          @RequestParam String username) throws FeignException {
         return cartService.removeFromCart(products, username);
     }
 
     @Override
-    public ShoppingCartDto changeProductQuantity(ChangeProductQuantityRequest request, @RequestParam String username) throws FeignException {
+    public ShoppingCartDto changeProductQuantity(@RequestBody ChangeProductQuantityRequest request,
+                                                 @RequestParam String username) throws FeignException {
         log.info("Пользователь {} изменяет количество товара {} на {}.",
                 username,
                 request.getProductId(),
